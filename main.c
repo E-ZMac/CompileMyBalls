78b420dd651623771ad3b95a52a4106cb9c4af93
ElijahMac <kilobolt1@mail.com>
2025-08-09 19:37:01 -0500

    Added Token Structures

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
-
+#include <stdio.h>
+#include "helpers/vector.h"
+#include "compiler.h"
 
 int main() {
+    int res = compileFile("./test.c", "./test", 0);
+    if (res == COMPILER_FILE_COMPILED_OK) {
+        printf("Compile completed with no errors\n");
+    } else if (res == COMPILER_FAILED_WITH_ERRORS) {
+        printf("Compile failed\n");
+    } else {
+        printf("Unkown response for compile file");
+    }
 
     return 0;
 }
